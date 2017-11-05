544a58f82a43c1bb9f679a72024233d652966412
Jozef Cipa <cipa.jozef@gmail.com>
2017-11-05 15:11:56 +0100

    CONFIG-added autoprefixer to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@ var sass = require('gulp-ruby-sass');
 var notify = require('gulp-notify');
 var cleanCSS = require('gulp-clean-css');
 var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
 
 //SASS
 gulp.task('build:sass', () => {
         sass('sass/*.sass')
         	.on('error', sass.logError)
 	        .pipe(cleanCSS())
+	        .pipe(autoprefixer({
+	            cascade: false
+	        }))
 			.pipe(notify({
 				message: "SASS compiled."
 			}))
