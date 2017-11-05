c363b0c791e5cf610943f8e12d470f26c79d6720
Jozef Cipa <cipa.jozef@gmail.com>
2017-11-05 16:20:15 +0100

    FEATURE-added animations, edited texts, small UI changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,24 +1,49 @@
 $(document).ready(function () {
+
+	// hide sections
+	$('#about-me, #why, #skills, #experience, #education').css('opacity', 0);
+	$('#skills .content div').css('opacity', 0);
 	
 	// title update
 	var $title = $('title');
 	var title = $title.html();
-	$(window).on('blur', function () {
-		$title.html('I miss you 😞, come back!');
-	});
-	$(window).on('focus', function () {
-		$title.html(title);
-	});
+	$(window)
+		.on('blur', function () {
+			$title.html('So, what do you think ?');
+		})
+		.on('focus', function () {
+			$title.html(title);
+		});
 
-	// sections opacity when scrolling
 	$(window).on('load scroll', function () {
 
-		var windowHeight = $(window).height();
-		var position = $(window).scrollTop();
+		// fade out landing section 
+		var windowHeight = $(this).height();
+		var position = $(this).scrollTop();
+		$('#landing').css('opacity', (1 - (position / windowHeight) * 1.1));
 
-		if (position < 0) return;
+		// fading in sections on scrolling
+		$('#about-me, #why, #skills, #experience, #education').animateDisplay();
 
-		$('#landing').css('opacity', (1 - (position / windowHeight) * 1.01));
-		$('#about-me').css('opacity', (position / windowHeight) * 1.3);
+		// fading skills
+		$('#skills .content div').animateDisplay(2/15);
 	});
 });
+
+$.fn.animateDisplay = function (positionRatio) {
+
+	if (positionRatio == undefined) {
+		positionRatio = 1 / 3; // animation triggers when 1/3 of section is scrolled into window
+	}
+
+	return $(this).each(function () {
+		var $window = $(window);
+
+		var sectionStart = $(this).position().top;
+		var currentScrollPosition = $window.scrollTop();
+
+		if (currentScrollPosition > sectionStart - ($window.height() * (1 - positionRatio))) {
+			$(this).addClass('animated fadeIn');
+		}
+	});
+}
